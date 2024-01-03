35864c6551badb59db0e9b9bea7b99ed7f737021
AurelioVill <aurelio.lp@hotmail.com>
2024-01-03 20:36:29 -0300

    Adição de comentários

--- python/aula2.py
+++ python/aula2.py
@@ -1,2 +1,19 @@
+#---------------------------------------------------
+#Aula de  print (seprador(sep), end(o que vai no final do argumento))
+
 print (12, 34, sep= '-', end= '#\n')
 print (56, 78, sep= '-', end= '*\n')
+
+
+
+#---------------------------------------------------
+#Aula de Strings
+
+#Aspas  simples
+print('José Aurélio')
+
+#Aspas duplas
+print("José Aurélio")
+
+#Escape
+print("José \"Aurélio") 
