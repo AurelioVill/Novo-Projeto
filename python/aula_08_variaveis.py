6fb9984c21023a7d5f65d1facffb0f2f3a892a9b
AurelioVill <aurelio.lp@hotmail.com>
2024-01-16 20:44:01 -0300

    Praticando criação de variáveis

---
+++ python/aula_08_variaveis.py
@@ -0,0 +1,15 @@
+nome = 'Aurélio'
+sobrenome = 'Vilar'
+idade = 31
+ano_nascimento = 2024 - 31
+maior_de_idade = idade >= 18
+altura_metros = 1.75
+
+print('Nome-->', nome)
+print('Sobrenome-->', sobrenome)
+print('Idade-->', idade)
+print('Ano de nascimento-->', ano_nascimento)
+print('È maior de idade?-->', maior_de_idade)
+print('Altura em metros-->', altura_metros)
+
+
