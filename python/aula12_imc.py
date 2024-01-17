0d318f66d2f8882f87c7b4957e6c3247417b8518
AurelioVill <aurelio.lp@hotmail.com>
2024-01-17 20:31:46 -0300

    Calculo IMC

---
+++ python/aula12_imc.py
@@ -0,0 +1,7 @@
+nome = 'Aurelio'
+altura = 1.75
+peso = 85
+imc = peso / altura ** 2
+
+print(nome + ' tem ' + str(altura) + ' de altura, ' + ' pesa' + str(peso) + ' quilos e seu IMC é: ' + str(imc))
+
