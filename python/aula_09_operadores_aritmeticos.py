fafa826d101e14839ae41c3e1972dd3c001ff430
AurelioVill <aurelio.lp@hotmail.com>
2024-01-16 21:24:13 -0300

    Praticando os operadores aritimeticos

---
+++ python/aula_09_operadores_aritmeticos.py
@@ -0,0 +1,21 @@
+adicao = 10 +10
+print('Adição:', adicao)
+
+subtracao = 10 - 5
+print('Subtração:', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação:', multiplicacao)
+
+divisao = 10 / 2
+print('Divisão:', divisao)  #sempre traz float
+
+divisao_inteira = 10 // 3
+print('Divisão inteira:', divisao_inteira)  # trava o final do número, não há disima periodica
+
+modulo = 16 % 8 
+print('Modulo:', modulo)  # verificar se o numero é divisível por outro, mostra o resto
+                            #sendo 1 nãi é divisivel, restando 0 é dividivel
+
+
+
