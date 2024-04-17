a46578b91dc1444ccf7307c653f6dd3ee0e738d9
AurelioVill <aurelio.lp@hotmail.com>
2024-04-17 11:33:56 -0300

    Aula12
    Formatação de Strings

--- python/aula12_imc.py
+++ python/aula12_imc.py
@@ -1,7 +1,15 @@
 nome = 'Aurelio'
 altura = 1.75
-peso = 85
+peso = 84
 imc = peso / altura ** 2
 
-print(nome + ' tem ' + str(altura) + ' de altura, ' + ' pesa' + str(peso) + ' quilos e seu IMC é: ' + str(imc))
+# f-strings (formatação de strings)
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
 
+#print(nome + ' tem ' + str(altura) + ' de altura, ' + ' pesa' + str(peso) + ' quilos e seu IMC é: ' + str(imc))
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
